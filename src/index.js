9c26437b83f16fb121c10d00bafdf69508dadca6
Kop1tes <prsasha0@gmail.com>
2022-12-22 12:06:32 +0200

    add second change

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './css/styles.css';
-import { Notify } from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import Notiflix from 'notiflix';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const axios = require('axios').default;
 
@@ -12,16 +14,30 @@ const refs = {
 }
 
 let page = 1;
+let query = '';
 const API_KEY = '32216625-6f7cdca1cd8ffe8b3abf6c6b4';
 const BASE_URL = 'https://pixabay.com/api/';
 
 refs.searchForm.addEventListener('submit', formSubmit);
-refs.gallery.addEventListener('click', onClick);
+refs.btnLoadMore.addEventListener('click', onLoadMore)
+// refs.gallery.addEventListener('click', onClick);
+
 
 function formSubmit(e) {
     e.preventDefault();
 
+    const query = e.currentTarget.elements.searchQuery.value.trim();
+    if (query === "") {
+        return;
+    }
+
+    const url = `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+    fetch(url).then(r => r.json()).then(console.log);
 }
 
-fetch('https://pixabay.com/api/?key=32216625-6f7cdca1cd8ffe8b3abf6c6b4&q=flowers&image_type=photo&orientation=horizontal&safesearch=true').then(r => r.json()).then(console.log);
+function onLoadMore() {  
+    const url = `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
 
+    fetch(url).then(r => r.json()).then(console.log);
+}
